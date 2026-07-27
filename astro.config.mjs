8676c22b7b865a6b173dba1793fc547320cbbf0e
Tony Karnauch <tonquy11@gmail.com>
2026-07-27 19:45:38 -0400

    Update site URL in configuration and robots.txt to reflect new domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://budgetautosrepair.com',
   output: 'server',
   vite: {
     plugins: [tailwindcss()]
